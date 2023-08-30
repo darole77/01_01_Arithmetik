c079e3a469a0ce2c0e34d0d6d4de923bd88d58f4
darole77 <darole77@yahoo.de>
2023-08-30 20:43:15 +0200

    first commit

---
+++ .ipynb_checkpoints/01_Arithmetik-checkpoint.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+from PIL import Image
+
+
+st.set_page_config(
+    page_title="01_Arithmetik")
+
+st.title("Grundlagen der Arithmetik")
+st.sidebar.success("Select a page above.")
