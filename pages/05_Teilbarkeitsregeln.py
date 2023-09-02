42f098a9aa9191f3a884492c3cd526ddf6ffcf75
darole77 <142713605+darole77@users.noreply.github.com>
2023-09-02 09:24:34 +0200

    Update 05_Teilbarkeitsregeln.py

--- pages/05_Teilbarkeitsregeln.py
+++ pages/05_Teilbarkeitsregeln.py
@@ -9,9 +9,13 @@ image_url = "https://imageshack.com/i/pmpxMV2Qj"
 st.image(image_url)
 
 st.markdown("Eine **Primzahl** ist eine natürliche Zahl, die **genau zwei natürliche Teiler** hat.)
+image_url = "https://i.postimg.cc/CLcDgxTG/01-01-Primzahlen.jpg"
+st.image(image_url)
+st.markdown("")            
 st.markdown("Die ersten zehn Primzahlen sind:")
 st.latex(r"""\mathrm{2, \: 3, \: 5, \: 7, \: 11, \: 13, \: 17, \: 19, \: 23, \:, 29, \: ...}""")
 
+            
 st.markdown("")
 st.markdown("")
 st.markdown("")
@@ -22,3 +26,10 @@ with col1:
     st.image(image_url, width=200)
 with col2:
     st.markdown("[Teilbarkeitsregeln](https://www.learningsnacks.de/share/249456/299dc33dbfdf78fbddc02610cfcfcffabaceb33d)")
+
+col1, col2 = st.columns(2)
+with col1:
+    image_url = "https://i.postimg.cc/m2xLjR7y/Logo-LS.jpg"
+    st.image(image_url, width=200)
+with col2:
+    st.markdown("[Primzahlen](https://www.learningsnacks.de/share/249461/fc7e3d72b0b0e02e19046e59b1ba1246b16bb937)")
