d1e1bcdcdd3f3c8cb4b7cbc344ab77f508c915c9
darole77 <142713605+darole77@users.noreply.github.com>
2023-09-02 09:16:38 +0200

    Update 05_Teilbarkeitsregeln.py

--- pages/05_Teilbarkeitsregeln.py
+++ pages/05_Teilbarkeitsregeln.py
@@ -4,9 +4,13 @@ import pandas as pd
 from tabulate import tabulate
 from PIL import Image
 
-st.header("Teilbarkeitsregeln")
+st.header("Teilbarkeitsregeln und Primzahlen")
 image_url = "https://imageshack.com/i/pmpxMV2Qj"
 st.image(image_url)
+
+st.markdown("Eine **Primzahl** ist eine natürliche Zahl, die **genau zwei natürliche Teiler** hat.)
+st.markdown("Die ersten zehn Primzahlen sind:")
+st.latex(r"""\mathrm{2, \: 3, \: 5, \: 7, \: 11, \: 13, \: 17, \: 19, \: 23, \:, 29, \: ...}""")
 
 st.markdown("")
 st.markdown("")
