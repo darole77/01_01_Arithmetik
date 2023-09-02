e96c379584a3ca9e799fce3c24f1cf056b259dcd
darole77 <142713605+darole77@users.noreply.github.com>
2023-09-02 08:53:08 +0200

    Update 10_Rechenregeln_für_rationale Zahlen.py

--- pages/10_Rechenregeln_für_rationale Zahlen.py
+++ pages/10_Rechenregeln_für_rationale Zahlen.py
@@ -29,6 +29,28 @@ st.write("")
 image_url = "https://imageshack.com/i/pnYUXQiSj"
 st.image(image_url)
 
-st.markdown("[Learningsnack zum Kürzen und Erweitern von Brüchen](https://www.learningsnacks.de/share/251533/a1046b648696ab110cfe4c3df90cd6c525684b3d ")
-st.markdown("[Learningsnack zum Addieren und Subtrahieren von Brüchen](https://www.learningsnacks.de/share/251550/e68247cb48b7c134df22a4afe866edd649bfc993")
-st.markdown("[Learningsnack zum Multiplizieren und Dividieren von Brüchen](https://www.learningsnacks.de/share/251570/da0479fa4b4a1c0081e897fcc8f08ebdf30c925a")
+st.markdown("")
+st.markdown("")
+st.markdown("")
+st.markdown("")
+col1, col2 = st.columns(2)
+with col1:
+    image_url = "https://i.postimg.cc/m2xLjR7y/Logo-LS.jpg"
+    st.image(image_url, width=200)
+with col2:
+    st.markdown("[Kürzen und Erweitern von Brüchen](https://www.learningsnacks.de/share/251533/a1046b648696ab110cfe4c3df90cd6c525684b3d)")
+
+col1, col2 = st.columns(2)
+with col1:
+    image_url = "https://i.postimg.cc/m2xLjR7y/Logo-LS.jpg"
+    st.image(image_url, width=200)
+with col2:
+    st.markdown("[Addieren und Subtrahieren von Brüchen](https://www.learningsnacks.de/share/251550/e68247cb48b7c134df22a4afe866edd649bfc993)")
+
+col1, col2 = st.columns(2)
+with col1:
+    image_url = "https://i.postimg.cc/m2xLjR7y/Logo-LS.jpg"
+    st.image(image_url, width=200)
+with col2:
+    st.markdown("[Multiplizieren und Dividieren von Brüchen](https://www.learningsnacks.de/share/251570/da0479fa4b4a1c0081e897fcc8f08ebdf30c925a)")
+
