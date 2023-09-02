699437a9818cfd5251f2e6293f4ce2ea546aec75
darole77 <142713605+darole77@users.noreply.github.com>
2023-09-02 08:50:25 +0200

    Update 07_Rechenregeln_für_ganze_Zahlen.py

--- pages/07_Rechenregeln_für_ganze_Zahlen.py
+++ pages/07_Rechenregeln_für_ganze_Zahlen.py
@@ -13,4 +13,13 @@ st.image(image_url)
 image_url = "https://imageshack.com/i/pmHQNTRlj"
 st.image(image_url)
 
-st.markdown("[Learningsnack zu den Rechenregeln für ganze Zahlen](https://www.learningsnacks.de/share/251220/fc605d7701749d3caf676ee3de25edb49d1dc213)")
+st.markdown("")
+st.markdown("")
+st.markdown("")
+st.markdown("")
+col1, col2 = st.columns(2)
+with col1:
+    image_url = "https://i.postimg.cc/m2xLjR7y/Logo-LS.jpg"
+    st.image(image_url, width=200)
+with col2:
+    st.markdown("[Rechenregeln für ganze Zahlen](https://www.learningsnacks.de/share/251220/fc605d7701749d3caf676ee3de25edb49d1dc213)")
