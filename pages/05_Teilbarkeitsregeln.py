8e9fb857b71c78305e6d83ab82bfc64aafc99ebe
darole77 <142713605+darole77@users.noreply.github.com>
2023-09-02 08:48:59 +0200

    Update 05_Teilbarkeitsregeln.py

--- pages/05_Teilbarkeitsregeln.py
+++ pages/05_Teilbarkeitsregeln.py
@@ -8,4 +8,13 @@ st.header("Teilbarkeitsregeln")
 image_url = "https://imageshack.com/i/pmpxMV2Qj"
 st.image(image_url)
 
-st.markdown("[Learningsnack zu den Teilbarkeitsregeln](https://www.learningsnacks.de/share/249456/299dc33dbfdf78fbddc02610cfcfcffabaceb33d)")
+st.markdown("")
+st.markdown("")
+st.markdown("")
+st.markdown("")
+col1, col2 = st.columns(2)
+with col1:
+    image_url = "https://i.postimg.cc/m2xLjR7y/Logo-LS.jpg"
+    st.image(image_url, width=200)
+with col2:
+    st.markdown("[Teilbarkeitsregeln](https://www.learningsnacks.de/share/249456/299dc33dbfdf78fbddc02610cfcfcffabaceb33d)")
